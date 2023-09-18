fc78b39e6bdc7cf2d5eebb30fb2c2a59f88ed3e9
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-18 15:43:54 -0300

    add model refresh_token

--- src/Routers/Culto/PresencaCulto.ts
+++ src/Routers/Culto/PresencaCulto.ts
@@ -2,14 +2,12 @@ import { FastifyInstance } from "fastify";
 import { PresencaCultoController } from '../../Controllers/Culto';
 
 const routerPresencaCulto = async (fastify: FastifyInstance) => {
-  // ESCOLA
   fastify.get("/presencacultos", PresencaCultoController.index);
   fastify.get('/presencacultos/:id', PresencaCultoController.show);
   fastify.get(`/presencacultosbycelula/:culto/:lider`, PresencaCultoController.searchByIdCulto);
   fastify.post("/presencacultos", PresencaCultoController.store);
   fastify.delete("/presencacultos/:id", PresencaCultoController.delete);
   fastify.put("/presencacultos/:id", PresencaCultoController.update);
-  // fastify.post("/presencamembros", PresencaCultoController.many);
 
 };
 
