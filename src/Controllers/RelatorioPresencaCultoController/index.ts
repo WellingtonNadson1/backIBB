0488ee71944f66d4ff7e98d3d22be21fee6af390
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-22 22:08:08 -0300

    init report presence culto supervision

--- src/Controllers/RelatorioPresencaCultoController/index.ts
+++ src/Controllers/RelatorioPresencaCultoController/index.ts
@@ -4,6 +4,10 @@ import { PresencaCultoRepositorie } from "../../Repositories/Culto";
 // import createPDFRelatorioPresenceCultoSupervision from "../../functions/createPDFRelatorioPresenceCultoSupervision";
 import { TDocumentDefinitions } from "pdfmake/interfaces";
 import PdfPrinter from "pdfmake";
+import dayjs from "dayjs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 const RelatorioPresencaCultoDataSchema = object({
   status: boolean(),
@@ -19,7 +23,25 @@ interface RelatorioPresencaCultoParams {
   id: string;
 }
 
+const res = async () => { await prisma.supervisao.
+  findFirst({
+    where: {
+      supervisor: {
+        id: '31c33e5e-282a-45ed-a00f-6a7a40abb6b2'
+      },
+    },
+  select: {
+  supervisor: {
+    select: {
+      first_name: true
+    }
+  }
+  }
+})
+}
+
 class RelatorioPresencaCultoController {
+
   // Fazendo uso do Fastify
   async index(request: FastifyRequest, reply: FastifyReply) {
     const fonts = {
@@ -31,23 +53,30 @@ class RelatorioPresencaCultoController {
       },
     }
     const printer = new PdfPrinter(fonts)
-  
+
     const docDefinitions: TDocumentDefinitions = {
       defaultStyle: { font: "Helvetica" },
       content: [
-        { text: "Meu Segundo Relatório!" }
+        {
+          pageOrientation: 'landscape',
+          table: {
+            body: [
+            res && [`SUPERVISOR(A): ${res}`, `MÊS: ${dayjs().month()} - ${dayjs().year()}`]
+            ]
+          }
+        }
       ],
     }
-  
+
     const pdfDoc = printer.createPdfKitDocument(docDefinitions)
-  
+
     const chunks: any[] = []
-  
+
     pdfDoc.on("data", (chunk) => {
       chunks.push(chunk)
     })
     pdfDoc.end()
-  
+
     pdfDoc.on("end", () => {
       const result = Buffer.concat(chunks)
       reply.header('Content-Type', 'application/pdf')
@@ -70,21 +99,21 @@ class RelatorioPresencaCultoController {
     return reply.code(200).send(presencasReuniaoCelula);
   }
 
-  async searchByIdCulto(
-    request: FastifyRequest<{
-      Params: RelatorioPresencaCultoParams;
-    }>,
-    reply: FastifyReply
-  ) {
-    const presenca_culto = request.params.id;
+  // async searchByIdCulto(
+  //   request: FastifyRequest<{
+  //     Params: RelatorioPresencaCultoParams;
+  //   }>,
+  //   reply: FastifyReply
+  // ) {
+  //   const presenca_culto = request.params.id;
 
-    const presencaCultoIsRegister =
-      await PresencaCultoRepositorie.findByIdCulto(presenca_culto);
-    if (!presencaCultoIsRegister) {
-      return reply.code(404).send({ message: "Presença not Register!" });
-    }
-    return reply.code(200).send(presencaCultoIsRegister);
-  }
+  //   const presencaCultoIsRegister =
+  //     await PresencaCultoRepositorie.findByIdCulto(presenca_culto);
+  //   if (!presencaCultoIsRegister) {
+  //     return reply.code(404).send({ message: "Presença not Register!" });
+  //   }
+  //   return reply.code(200).send(presencaCultoIsRegister);
+  // }
 
 }
 
