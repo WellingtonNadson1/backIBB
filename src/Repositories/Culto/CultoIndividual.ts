9bca381bf3371c8f5384ca9965e29fe8ea6fdbfe
Wellington Nadson <wellingtonnadson@MacBook-Air-de-Wellington.local>
2024-03-31 10:50:28 -0300

    Add anoe day for seach culto individual in query

--- src/Repositories/Culto/CultoIndividual.ts
+++ src/Repositories/Culto/CultoIndividual.ts
@@ -1,6 +1,7 @@
 import { Prisma } from "@prisma/client";
 import { CultoIndividualData } from "../../Controllers/Culto/CultoIndividual";
 import { createPrismaInstance, disconnectPrisma } from "../../services/prisma";
+import dayjs from "dayjs";
 
 type UpdateCultoIndividualInput = Prisma.CultoIndividualUpdateInput & {
   presencas_culto?: { connect: { id: string } }[];
@@ -20,108 +21,108 @@ class CultoIndividualRepositorie {
 
     try {
       const result = await prisma?.cultoIndividual.findMany({
-          where: {
-              data_inicio_culto: {
-                  gte: new Date(startDate),
-                  lte: new Date(endDate),
-              },
-              presencas_culto: {
-                  some: {
-                      membro: {
-                        supervisao_pertence: {
-                          id: { equals: superVisionId }
-                        },
-                      }
-                  }
-              }
+        where: {
+          data_inicio_culto: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
           },
-          orderBy: {
-            data_inicio_culto: 'asc' // Ordena em ordem crescente
+          presencas_culto: {
+            some: {
+              membro: {
+                supervisao_pertence: {
+                  id: { equals: superVisionId }
+                },
+              }
+            }
+          }
         },
-          select: {
+        orderBy: {
+          data_inicio_culto: 'asc' // Ordena em ordem crescente
+        },
+        select: {
+          id: true,
+          data_inicio_culto: true,
+          presencas_culto: {
+            where: {
+              membro: {
+                supervisao_pertence: {
+                  id: { equals: superVisionId }
+                },
+              }
+            },
+            select: {
               id: true,
-              data_inicio_culto: true,
-              presencas_culto: {
-                  where: {
-                      membro: {
-                        supervisao_pertence: {
-                          id: { equals: superVisionId }
-                        },
-                      }
-                  },
-                  select: {
-                      id: true,
-                      status: true,
-                      date_create:true,
-                      membro: {
-                          select: {
-                              id: true,
-                              first_name: true,
-                              presencas_cultos: false,
-                              supervisao_pertence: {
-                                  select: {
-                                      id: true,
-                                      nome: true,
-                                  }
-                              },
-                              celula: {
-                                  select: {
-                                      id: true,
-                                      nome: true,
-                                  }
-                              }
-                          }
-                      }
-                  }
-              },
-              culto_semana: {
-                  select: {
+              status: true,
+              date_create: true,
+              membro: {
+                select: {
+                  id: true,
+                  first_name: true,
+                  presencas_cultos: false,
+                  supervisao_pertence: {
+                    select: {
                       id: true,
                       nome: true,
+                    }
                   },
-              },
+                  celula: {
+                    select: {
+                      id: true,
+                      nome: true,
+                    }
+                  }
+                }
+              }
+            }
           },
-        });
-        const totalCultosPeriodo = result.length
-        const cultoPrimicia = result.reduce((total, primicia) => {
-          return total + (primicia.culto_semana?.id === 'bffb62af-8d03-473a-ba20-ab5a9d7dafbe' ? 1 : 0);
-        }, 0);
+          culto_semana: {
+            select: {
+              id: true,
+              nome: true,
+            },
+          },
+        },
+      });
+      const totalCultosPeriodo = result.length
+      const cultoPrimicia = result.reduce((total, primicia) => {
+        return total + (primicia.culto_semana?.id === 'bffb62af-8d03-473a-ba20-ab5a9d7dafbe' ? 1 : 0);
+      }, 0);
 
-        const cultoDomingoSacrificio = result.reduce((total, sacrificio) => {
-          return total + (sacrificio.culto_semana?.id === 'e7bc72d1-8faa-4bbe-9c24-475b64f956cf' ? 1 : 0);
-        }, 0);
+      const cultoDomingoSacrificio = result.reduce((total, sacrificio) => {
+        return total + (sacrificio.culto_semana?.id === 'e7bc72d1-8faa-4bbe-9c24-475b64f956cf' ? 1 : 0);
+      }, 0);
 
-        const cultoQuarta = result.reduce((total, quarta) => {
-          return total + (quarta.culto_semana?.id === '4064be1d-bf55-4851-9f76-99c4554a6265' ? 1 : 0);
-        }, 0);
+      const cultoQuarta = result.reduce((total, quarta) => {
+        return total + (quarta.culto_semana?.id === '4064be1d-bf55-4851-9f76-99c4554a6265' ? 1 : 0);
+      }, 0);
 
-        const cultoSabado = result.reduce((total, sabado) => {
-          return total + (sabado.culto_semana?.id === '84acfbe4-c7e0-4841-813c-04731ffa9c67' ? 1 : 0);
-        }, 0);
+      const cultoSabado = result.reduce((total, sabado) => {
+        return total + (sabado.culto_semana?.id === '84acfbe4-c7e0-4841-813c-04731ffa9c67' ? 1 : 0);
+      }, 0);
 
-        const cultoDomingoManha = result.reduce((total, domingoManha) => {
-          return total + (domingoManha.culto_semana?.id === 'cab02f30-cade-46ca-b118-930461013d53' ? 1 : 0);
-        }, 0);
+      const cultoDomingoManha = result.reduce((total, domingoManha) => {
+        return total + (domingoManha.culto_semana?.id === 'cab02f30-cade-46ca-b118-930461013d53' ? 1 : 0);
+      }, 0);
 
-        const cultoDomingoTarde = result.reduce((total, domingoTarde) => {
-          return total + (domingoTarde.culto_semana?.id === 'ea08ec9b-3d1b-42f3-818a-ec53ef99b78f' ? 1 : 0);
-        }, 0);
+      const cultoDomingoTarde = result.reduce((total, domingoTarde) => {
+        return total + (domingoTarde.culto_semana?.id === 'ea08ec9b-3d1b-42f3-818a-ec53ef99b78f' ? 1 : 0);
+      }, 0);
 
-        return {
-          ...result,
-          cultoQuarta: cultoQuarta,
-          cultoPrimicia: cultoPrimicia,
-          cultoDomingoSacrificio: cultoDomingoSacrificio,
-          cultoSabado: cultoSabado,
-          cultoDomingoManha: cultoDomingoManha,
-          cultoDomingoTarde: cultoDomingoTarde,
-          totalCultosPeriodo: totalCultosPeriodo,
+      return {
+        ...result,
+        cultoQuarta: cultoQuarta,
+        cultoPrimicia: cultoPrimicia,
+        cultoDomingoSacrificio: cultoDomingoSacrificio,
+        cultoSabado: cultoSabado,
+        cultoDomingoManha: cultoDomingoManha,
+        cultoDomingoTarde: cultoDomingoTarde,
+        totalCultosPeriodo: totalCultosPeriodo,
       };
     }
     finally {
       await disconnectPrisma()
     }
-}
+  }
 
   async findAll() {
     const prisma = createPrismaInstance()
@@ -163,16 +164,17 @@ class CultoIndividualRepositorie {
   async findPerPeriod(firstDayOfMonth: Date, lastDayOfMonth: Date) {
     const prisma = createPrismaInstance()
     try {
+      const lastDayOfMonthPlusOneDay = dayjs(lastDayOfMonth).add(1, 'day');
       const result = await prisma?.cultoIndividual.findMany({
         where: {
           data_inicio_culto: {
-              gte: firstDayOfMonth,
-              lte: lastDayOfMonth,
+            gte: firstDayOfMonth,
+            lte: lastDayOfMonthPlusOneDay.toISOString(),
           },
-      },
-      orderBy: {
-        data_inicio_culto: 'asc' // Ordena em ordem crescente
-    },
+        },
+        orderBy: {
+          data_inicio_culto: 'asc' // Ordena em ordem crescente
+        },
         select: {
           id: true,
           data_inicio_culto: true,
@@ -251,14 +253,14 @@ class CultoIndividualRepositorie {
         },
       });
       // Conecte os relacionamentos opcionais, se fornecidos
-    if (data.culto_semana) {
-      await prisma?.cultoIndividual.update({
-        where: { id: cultoIndividual?.id },
-        data: {
-          culto_semana: { connect: { id: data.culto_semana } },
-        },
-      });
-    }
+      if (data.culto_semana) {
+        await prisma?.cultoIndividual.update({
+          where: { id: cultoIndividual?.id },
+          data: {
+            culto_semana: { connect: { id: data.culto_semana } },
+          },
+        });
+      }
 
       if (data.presencas_culto) {
         await prisma?.cultoIndividual.update({
@@ -281,10 +283,10 @@ class CultoIndividualRepositorie {
     try {
       const { data } = cultoIndividualDataForm;
       const updateCultoIndividualInput: UpdateCultoIndividualInput = {
-          data_inicio_culto: data.data_inicio_culto,
-          data_termino_culto: data.data_termino_culto,
-          status: data.status,
-          date_update: new Date(),
+        data_inicio_culto: data.data_inicio_culto,
+        data_termino_culto: data.data_termino_culto,
+        status: data.status,
+        date_update: new Date(),
       };
 
       // Conecte os relacionamentos opcionais apenas se forem fornecidos
